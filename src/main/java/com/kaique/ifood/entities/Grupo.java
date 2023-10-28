0ff1db890decaed960013efa80a8efb901928cbd
NakamuraSuzuki <devprodev777@gmail.com>
2023-10-27 21:28:45 -0300

    criação da classe grupo e mapeamento para permissao

---
+++ src/main/java/com/kaique/ifood/entities/Grupo.java
@@ -0,0 +1,41 @@
+package com.kaique.ifood.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter @Setter
+@Entity
+@Table(name = "tb_grupo")
+public class Grupo implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+
+	@ManyToMany
+	@JoinTable(name = "tb_grupo_permissao", 
+	joinColumns = @JoinColumn(name = "grupo_id"), 
+	inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+	private List<Permissao> permissoes = new ArrayList<>();
+}
