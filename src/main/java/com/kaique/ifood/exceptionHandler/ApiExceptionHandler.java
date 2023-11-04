dc4f3516361d96b8790a84e0a694bfb0f716afc2
NakamuraSuzuki <devprodev777@gmail.com>
2023-11-04 20:51:25 -0300

    Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler

--- src/main/java/com/kaique/ifood/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/kaique/ifood/exceptionHandler/ApiExceptionHandler.java
@@ -2,37 +2,49 @@ package com.kaique.ifood.exceptionHandler;
 
 import java.time.LocalDateTime;
 
-import org.springframework.beans.factory.parsing.Problem;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.kaique.ifood.exception.EntidadeEmUsoException;
 import com.kaique.ifood.exception.EntidadeNaoEncontradaException;
 import com.kaique.ifood.exception.NegocioException;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatusCode statusCode, WebRequest request) {
+		String ErrorManager = ((HttpStatus) statusCode).getReasonPhrase();
+
+		if (body == null) {
+			body = ApiErro.builder().dataHora(LocalDateTime.now()).mensagem(ErrorManager).build();
+		}else if(body instanceof String) {
+			body = ApiErro.builder().dataHora(LocalDateTime.now()).mensagem((String) body).build();
+
+		}
+		
+		return super.handleExceptionInternal(ex, body, headers, statusCode, request);
+	}
 
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> trataNegocioException(NegocioException e) {
-		ApiErro erro = ApiErro.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
-
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+	public ResponseEntity<?> trataNegocioException(NegocioException e, WebRequest request) {
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> trataEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
-		ApiErro erro = ApiErro.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
-
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	public ResponseEntity<?> trataEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e, WebRequest request) {
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> trataEntidadeEmUsoException(EntidadeEmUsoException e) {
-		ApiErro erro = ApiErro.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
-
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
+	public ResponseEntity<?> trataEntidadeEmUsoException(EntidadeEmUsoException e, WebRequest request) {
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 }
