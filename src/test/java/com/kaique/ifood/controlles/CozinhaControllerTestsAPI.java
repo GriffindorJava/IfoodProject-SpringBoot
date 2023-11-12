3a0fe2a736b76e0c9ae9ee378318918897790b8d
NakamuraSuzuki <devprodev777@gmail.com>
2023-11-11 21:31:59 -0300

    Implementando Testes de API com REST Assured e validando o código de status HTTP

---
+++ src/test/java/com/kaique/ifood/controlles/CozinhaControllerTestsAPI.java
@@ -0,0 +1,31 @@
+package com.kaique.ifood.controlles;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class CozinhaControllerTestsAPI {
+
+	@LocalServerPort
+	private int port;
+	
+	@Test
+	@DisplayName("testa se o código http que retorna é o 200")
+	public void deveRetornaStatus200QuandoConsultaCozinha() {
+		RestAssured.given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
+}
